d04e034dcb661b7033e02ae644d89cc63019bf77
prashant <prashantwaghmode05@gmail.com>
2024-07-31 17:21:18 +0530

    added global exception handler and validations

--- accounts/src/main/java/com/spark/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/spark/accounts/dto/AccountsDto.java
@@ -1,14 +1,21 @@
 package com.spark.accounts.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotNull(message = "AccountNumber can not be a null or empty")
+    @Min(value = 10, message = "AccountNumber must be 10 digits")
+    @Max(value = 10, message = "AccountNumber must be 10 digits")
+    //@Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 
 }
--- accounts/src/main/java/com/spark/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/spark/accounts/exception/GlobalExceptionHandler.java
@@ -1,17 +1,50 @@
 package com.spark.accounts.exception;
 
 import com.spark.accounts.dto.ErrorResponseDto;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler  extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(constraintViolation -> {
+            String propertyPath = constraintViolation.getPropertyPath().toString();
+            errors.put(propertyPath.substring(propertyPath.indexOf(".") + 1), constraintViolation.getMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorList = ex.getBindingResult().getAllErrors();
+
+        validationErrorList.forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName, validationMsg);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(CustomerAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException exception,
@@ -35,5 +68,17 @@ public class GlobalExceptionHandler  extends ResponseEntityExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
+                                                                                 WebRequest webRequest){
+        ErrorResponseDto errorResponseDTO = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
