0587fd352f801cc46f19aa3ad007796fd02f93e9
prashant <prashantwaghmode05@gmail.com>
2024-08-01 17:07:23 +0530

    added loans service

---
+++ loans/src/main/java/com/spark/loans/repository/LoansRepository.java
@@ -0,0 +1,16 @@
+package com.spark.loans.repository;
+
+import com.spark.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+
+    Optional<Loans> findByLoanNumber(String loanNumber);
+
+}
