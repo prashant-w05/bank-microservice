80838e658fc905ea347b0aa4184c6f63ed331c81
prashant <prashantwaghmode05@gmail.com>
2024-08-01 14:11:01 +0530

    added swagger documentation

--- accounts/src/main/java/com/spark/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/spark/accounts/AccountsApplication.java
@@ -1,11 +1,34 @@
 package com.spark.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Bank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Spring Boot",
+						email = "springboot@email.com",
+						url = "http://springboot.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://apache.com"
+
+				)
+
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
--- accounts/src/main/java/com/spark/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/spark/accounts/dto/AccountsDto.java
@@ -1,20 +1,34 @@
 package com.spark.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
+    @Schema(
+            description = "Account Number of Bank account", example = "3454433243"
+    )
     @NotNull(message = "AccountNumber can not be a null or empty")
     @Min(value = 10, message = "AccountNumber must be 10 digits")
     //@Max(value = 10, message = "AccountNumber must be 10 digits")
     //@Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of Bank account", example = "Savings"
+    )
     @NotEmpty(message = "AccountType can not be a null or empty")
     private String accountType;
 
+    @Schema(
+            description = "Bank branch address", example = "123 Pune"
+    )
     @NotEmpty(message = "BranchAddress can not be a null or empty")
     private String branchAddress;
 
